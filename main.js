536222d2caeb9b603c741c15ad0f3adeb8a28091
QA-Dimon <98879622+QA-Dimon@users.noreply.github.com>
2022-09-21 14:27:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const date = require('./date');
 
 function hello() {
-    const currentDate = date.currentDate('d MMMM yyyy - dddd'); 
+    const currentDate = date.currentDate('dd MMMM yyyy - dddd'); 
     console.log('Hello world. Today is ' + currentDate + '.');
 }
 
